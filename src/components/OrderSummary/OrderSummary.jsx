5cd5aea700ef56e7d987d862dd29abfbb7326dca
Harish Bathini <bathiniharish7@gmail.com>
2025-07-17 19:07:42 +0530

    table height in cart page is fixed

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -1,41 +1,51 @@
-import React from 'react'
-import styles from './OrderSummary.module.css'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import styles from './OrderSummary.module.css';
+import { useSelector } from 'react-redux';
 import { Button } from '@mui/material';
-function OrderSummary() {
 
-    const cartProducts = useSelector((state)=>state.cart.products);
-    const totalAmount=cartProducts.reduce((acc,current)=>{
-        return acc = acc + current.price;
-    },0).toFixed(2)
+function OrderSummary() {
+  const cartProducts = useSelector((state) => state.cart.products);
+  const totalAmount = cartProducts.reduce((acc, current) => acc + current.price, 0).toFixed(2);
 
   return (
     <div className={styles.orderSummary}>
-        <h3 className={styles.title}>Order Summary</h3>
+      <h3 className={styles.title}>Order Summary</h3>
+
+<div>
+          <table className={styles.table}>
+        <thead>
+          <tr className={styles.heading}>
+            <td>Sl.No</td>
+            <td>Product Name</td>
+            <td>Price</td>
+          </tr>
+        </thead>
+      </table>
+
+      <div className={styles.scrollWrapper}>
         <table className={styles.table}>
-            <thead>
-                <tr className={styles.heading}>
-                    <td>Sl.No</td>
-                    <td>Product Name</td>
-                    <td>Price</td>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    cartProducts.map((product,index)=><tr className={styles.tableRow}>
-                        <td>{index+1}</td>
-                        <td>{product.title}</td>
-                        <td>₹{product.price}/-</td>
-
-                    </tr>)
-                }
-            </tbody>
+          <tbody>
+            {cartProducts.map((product, index) => (
+              <tr className={styles.tableRow} key={index}>
+                <td>{index + 1}</td>
+                <td>{product.title}</td>
+                <td>₹{product.price}/-</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
+      </div>
+</div>
 
-        <Button variant='contained' onClick={()=>alert("this feature is under development")}  className={styles.payButton}>Pay Total Amount : ₹{totalAmount}/-</Button>
-      
+      <Button
+        variant="contained"
+        onClick={() => alert('This feature is under development')}
+        className={styles.payButton}
+      >
+        Pay Total Amount : ₹{totalAmount}/-
+      </Button>
     </div>
-  )
+  );
 }
 
-export default OrderSummary
+export default OrderSummary;
