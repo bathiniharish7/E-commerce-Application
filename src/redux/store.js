5a195c9ea902c31a1dd379cb52dc98bff1e172f8
Harish Bathini <bathiniharish7@gmail.com>
2026-02-24 00:18:05 +0530

    persist cart items by syncing Redux store with localStorage and restoring on app load

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,31 @@ import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from "./cartSlice/cartSlice"
 import filterReducer from "./filterSlice/filterSlice"
 
+const loadCartData = () => {
+  try {
+    const data = localStorage.getItem("Shop-Data")
+    return data ? JSON.parse(data) : undefined
+  } catch {
+    return undefined
+  }
+}
+
 export const store = configureStore({
   reducer: {
-    cart:cartReducer,
+    cart: cartReducer,
     // filters:filterReducer
   },
+   preloadedState: {
+    cart: loadCartData() || { cartItems: {} }
+  }
+})
+
+store.subscribe(() => {
+  try {
+    const state = store.getState()
+    localStorage.setItem("Shop-Data", JSON.stringify(state.cart))
+  } catch (e){
+    console.log('Something went wrong',e);
+    
+  }
 })
